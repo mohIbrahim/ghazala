bc0193641e456bb1aa79e575b7450dd2899bb74e
Mohammed Ibrahim <mohibrahimqop@gmail.com>
2017-07-12 16:13:03 +0200

    daily commit ' :

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\JobsRequest;
 
 class JobsController extends Controller
 {
@@ -32,7 +33,7 @@ class JobsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(JobsRequest $request)
     {
         //
     }
@@ -66,7 +67,7 @@ class JobsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(JobsRequest $request, $id)
     {
         //
     }
