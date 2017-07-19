d572fbe832f467770191deaefb715545b011a367
Mohammed Ibrahim <mohibrahimqop@gmail.com>
2017-07-19 16:16:14 +0200

    Daily commit

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,55 +1,46 @@
 @extends('layouts.app')
 @section('title')
-	عرض كل أعضاء عائلات المُلاّك
+	عرض كل الوظائف
 @endsection
 @section('content')
 	<div class="">
 		
 		<div class="panel panel-primary">
 			<div class="panel-heading">
-				<h3 class="panel-title text-center"><strong>عرض كل أعضاء عائلات المُلاّك</strong></h3>
+				<h3 class="panel-title text-center"><strong>عرض كل الوظائف</strong></h3>
 			</div>
 			<div class="panel-body">
 				<div class="table-responsive">
 					<table class="table table-hover">
 						<thead>
 							<tr>								
-								<th>تعديل</th>
+							
 								<th>تاريخ و وقت التعديل</th>
 								<th>تاريخ و وقت الإنشاء</th>
 								<th>إنشاء من قبل المستخدم</th>
 								
-								<th>سن العضو</th>
-								<th>اسم المالك</th>
-								<th>اسم العضو</th>
+								<th>التعليقات</th>
+								<th>وصف الوظيفة</th>
+								<th>اسم الوظيفة</th>
 							</tr>
 							
 						</thead>
 						<tbody>
-							@foreach($members as $member)
+							@foreach($jobs as $job)
 								<tr>
+									
+									<td>{{ $job->updated_at }}</td>
+									<td>{{ $job->created_at }}</td>
 									<td>
-										@if(in_array('update_owners_family_members', $permissions))
-											<a href="{{action('OwnersFamilyMembersController@edit',['slug'=>$member->slug])  }}">تعديل</a>
-										@endif
-									</td>
-									<td>{{ $member->updated_at }}</td>
-									<td>{{ $member->created_at }}</td>
-									<td>
-										@if($member->creator)
-											{{ $member->creator->name }}
+										@if($job->creator)
+											{{ $job->creator->name }}
 										@endif
 									</td>									
+									
+									<td>{{ $job->comments }}</td>								
+									<td>{{ $job->descriptions }}</td>								
 									<td>
-										@if($member->date_of_birth)
-											{{ $member->date_of_birth->age }}
-										@endif
-									</td>
-									<td>
-										<a href="{{ action('OwnersController@show', ['slug'=>$member->owner->slug]) }}" target="_blank">{{ $member->owner->name }}</a>
-									</td>									
-									<td>
-										<a href="{{ action('OwnersFamilyMembersController@show', ['slug'=>$member->slug]) }}" target="_blank">{{ $member->name }}</a>
+										<a href="{{ action('JobsController@show', ['id'=>$job->id]) }}" target="_blank">{{ $job->name }}</a>
 									</td>
 									
 								</tr>
@@ -58,7 +49,7 @@
 						</tbody>
 					</table>
 				</div>
-						{{ $members->links() }}
+						{{ $jobs->links() }}
 			</div>
 		</div>
 		
